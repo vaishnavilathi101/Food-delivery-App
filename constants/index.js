2fd71e095a3ca005164ab9eb55224d817b5129fb
vaishnavilathi101 <68653542+vaishnavilathi101@users.noreply.github.com>
2021-11-25 19:32:14 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+import icons from "./icons";
+import images from "./images";
+import { categoryData, restaurantData } from "./data";
+
+import { COLORS, SIZES, FONTS } from "./theme";
+
+export { icons, images, COLORS, SIZES, FONTS, categoryData, restaurantData };
---
+++ screens/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home"
+import Restaurant from "./Restaurant"
+
+export {
+    Home,
+    Restaurant
+}
